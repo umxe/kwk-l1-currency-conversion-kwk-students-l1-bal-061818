337531a21e04a2b61e59e8bdefdb7d5301976305
umxe <u_habiba@rocketmail.com>
2018-06-19 19:21:19 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,76 @@
 # Write your cod here
+def usd_to_eur(num)
+  num /= 1.2 
+  return num.to_f
+end
+
+def eur_to_usd(num)
+  num *= 1.2
+  return num.to_f
+end
+
+def usd_to_jpy(num)
+  num *= 110
+  return num.to_f
+end
+
+def usd_to_g8p(num)
+  num *= 359.5
+  return num.to_f
+end
+
+def usd_to_aud(num)
+  num *= 1.36
+  return num.to_f
+end
+
+def jpy_to_usd(num)
+  num /= 110
+  return num
+end
+
+def g8p_to_usd(num)
+  num /= 359.5
+  return num.to_f
+end
+
+def aud_to_usd(num)
+  num /= 1.36
+  return num.to_f
+end
+
+puts "How much money do you want to convert?"
+amount = gets.chomp().to_i
+ 
+puts "Pick from the following menu:"
+puts "1. USD to EUR"
+puts "2. USD to JPY"
+puts "3. USD to GBP"
+puts "4. USD to AUD"
+puts "5. EUR to USD"
+puts "6. JPY to USD"
+puts "7. GBP to USD"
+puts "8. AUD to USD"
+ 
+choice = gets.chomp().to_i
+ 
+case choice
+when 1
+  puts usd_to_eur(amount)
+when 2
+  puts usd_to_jpy(amount)
+when 3
+  puts usd_to_g8p(amount)
+when 4
+  puts usd_to_aud(amount)
+when 5
+  puts eur_to_usd(amount)
+when 6
+  puts jpy_to_usd(amount)
+when 7
+  puts g8p_to_usd(amount)
+when 8
+  puts aud_to_usd(amount)
+else
+  puts "Invalid input, exiting..."
+end
